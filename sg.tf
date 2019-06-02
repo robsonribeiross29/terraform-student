860823ff211c9d7357ae197a9b26b6f314294505
Robson Ribeiro <robsont36@gmail.com>
2019-06-02 17:56:55 -0300

    add all traffic in subnet

--- sg.tf
+++ sg.tf
@@ -7,6 +7,12 @@ resource "aws_security_group" "sg_3389" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${var.cidr_subnet}"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
